d618737899323dfc84fd6e55d8f5de1a14fd0b14
pondycrane <pondycrane@gmail.com>
2014-11-21 22:38:59 +0800

    add photo upload functionality

--- lunchTime.js
+++ lunchTime.js
@@ -6,7 +6,117 @@ today = new Date();
 start = new Date(today.getFullYear(), today.getMonth(), today.getDate()); 
 end = new Date(today.getFullYear(), today.getMonth(), today.getDate()+1); 
 
+FpApiKey = new Meteor.Collection('fpapikey');
+FpFiles = new Meteor.Collection('fpfiles');
+FilepickerLoaded = false;
+
 if (Meteor.isClient) {
+
+  Meteor.startup(function () {
+    Session.set('fpfiles', []);
+    Session.set('apikey', null);
+
+    // saved apikey?
+    Tracker.autorun(function () {
+      var saved = FpApiKey.findOne({_id: 'apikey'});
+      if (saved && saved.val) {
+        Session.set('apikey', saved.val);
+      }
+      // autoloading via session or saved apikey?
+      if (Session.get('apikey')) {
+        loadFilePicker(Session.get('apikey'));
+      }
+    });
+
+    // setup reactive "loaded" variable
+    //   since the filepicker plugin doesn't "tell" us that
+    Tracker.autorun(function (filepicker) {
+      FilepickerLoaded = (typeof filepicker === "object");
+      Session.set('fploaded', (typeof filepicker === "object"));
+
+      var apikey = Session.get('apikey');
+      if (FilepickerLoaded && apikey && _.has(filepicker, 'setKey')) {
+        filepicker.setKey(apikey);
+      }
+    });
+  });
+
+
+	Template.picker.helpers({
+		apikey: function() {
+			return Session.get('apikey'); 
+		}, 
+		fploaded: function() {
+			return Session.get('fploaded'); 
+		}
+	}); 
+
+  Template.picker.events({
+    'submit form': function(event) {
+      event.preventDefault();
+      var apikey = $('#apikey').val();
+      if (apikey.length < 8) {
+        return false;
+      }
+      FpApiKey.upsert({_id: 'apikey'}, {_id: 'apikey', val: apikey});
+    },
+    'click .forget': function() {
+      Session.set('apikey', null);
+      FpApiKey.remove({_id: 'apikey'});
+      delete filepicker;
+    }
+  });
+
+
+
+  Template.lister.events({
+    'click .store': function() {
+      FpFiles.insert(this);
+      _.each(Session.get('fpfiles'), function(fpfile, i, l) {
+        if (fpfile.filename != this.filename) {
+          return;
+        }
+        delete l[i];
+        Session.set('fpfile', l);
+      });
+    }
+  });
+  Template.lister.helpers({
+    files: function() {
+      if (!Session.get('fpfiles')) {
+        return [];
+      }
+      return Session.get('fpfiles');
+    },
+    isImg: function() {
+     var fn = this.filename || '';
+     return String(fn).indexOf(/\.(png|gif|jpg|jped)$/i)
+    }
+  });
+
+  Template.stored.events({
+    'click .forget': function() {
+      if (!confirm('Are you sure?  (this does not delete the file, but forgets it from this screen)')) {
+        return;
+      }
+      FpFiles.remove(this._id);
+    }
+  });
+  Template.stored.helpers({
+    files: function() {
+      return FpFiles.find();
+    },
+    isImg: function() {
+     var fn = this.filename || '';
+     return String(fn).indexOf(/\.(png|gif|jpg|jped)$/i)
+    }
+  });
+
+
+
+
+
+
   // This code only runs on the client
   Template.body.helpers({
     tasks: function () {
@@ -139,7 +249,6 @@ Template.order.events({
                 }
         });
 }
-
 
 
 
